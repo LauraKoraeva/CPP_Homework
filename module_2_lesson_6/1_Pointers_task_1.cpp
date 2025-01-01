4da6d703b372b9d169ea7304e13475952a10d3e2
Laura <laurakoraeva@gmail.com>
2025-01-02 01:24:53 +0300

    edited

--- module_2_lesson_6/1_Pointers_task_1.cpp
+++ module_2_lesson_6/1_Pointers_task_1.cpp
@@ -5,25 +5,22 @@
 
 #include <iostream>
 
-void swapPointers (int *ptr_a, int *ptr_b)
+void swapPointers(int* ptr_a, int* ptr_b)
 {
     std::swap(*ptr_a, *ptr_b);
 }
-       
-       
+
 int main()
 {
     int a = 10;
     int b = 20;
-   
-    int * ptr_a = &a;
-    int * ptr_b = &b;
-   
+
+    int* ptr_a = &a;
+    int* ptr_b = &b;
+
     swapPointers(ptr_a, ptr_b);
-   
+
     std::cout << a << " " << b;
-   
+
     return 0;
 }
-
-
--- module_2_lesson_6/2_Pointers_task_2.cpp
+++ module_2_lesson_6/2_Pointers_task_2.cpp
@@ -5,30 +5,26 @@
 
 #include <iostream>
 
-void reverse(int *ptrArray, int length)
+void reverse(int* ptrArray, int length)
 {
-    for(int i = 0; i < length / 2; ++i)
+    for (int i = 0; i < length / 2; ++i)
     {
-       std::swap(ptrArray[i], ptrArray[length - i - 1]);
+        std::swap(ptrArray[i], ptrArray[length - i - 1]);
     }
-   
 }
- 
-       
+
 int main()
 {
     int length = 10;
     int array[10] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-   
-   
-   
+
     reverse(array, length);
-   
+
     for (int i = 0; i < length; ++i)
     {
         std::cout << array[i] << " ";
     }
-   
+
     return 0;
 }
 
