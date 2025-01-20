7e4800298e82b14940c6d1a7e445540e84b76ad7
Laura <laurakoraeva@gmail.com>
2025-01-21 00:58:58 +0300

    edited

---
+++ module_3_lesson_6/task_3.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <ctime>
+#include <iomanip>
+
+int main()
+{
+    std::cout << "Set the timer [MM:SS]: ";
+    std::tm timer;
+    std::cin >> std::get_time(&timer, "%M:%S");
+    std::time_t currentTime = std::time(nullptr);
+    std::time_t targetTime = currentTime + timer.tm_min * 60 + timer.tm_sec;
+
+    while (currentTime != targetTime)                                        // -1
+    {
+        std::time_t diff = targetTime - currentTime;
+        currentTime = std::time(nullptr);             
+        if (currentTime + diff != targetTime)      
+        {
+            diff = targetTime - currentTime;
+            int minutes = diff / 60;
+            int seconds = diff % 60;
+            std::cout << minutes / 10 << minutes % 10 << ":" << seconds / 10 << seconds % 10 << '\n';
+        }
+    }
+    std::cout << "DING! DING! DING!\n";
+
+    return 0;
+}
